4e88da8bbb58fdfd3d080c6dedcc3982263f2577
mifan <yanyn07@gmail.com>
2017-09-14 15:04:39 +0800

    Remove Nth Node From End of List
    Save the node address into an array

---
+++ RemoveNthNodeFromEndOfList.c
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+	struct ListNode **arr; //an array of list address
+    int i = 0;
+    
+    arr = (struct ListNode **)malloc(50*sizeof(struct ListNode *));
+   	arr[0] = head;
+    head = head->next;
+   	i++;
+    while (head != NULL)
+    {
+        arr[i] = (struct ListNode *)malloc(sizeof(struct ListNode));
+    	arr[i] = head;
+    	head = head->next;
+    	i++;
+    }
+    arr[i] = (struct ListNode *)malloc(sizeof(struct ListNode));
+    arr[i] = NULL;
+    
+    if (n == i) //remove the head
+    {
+        return arr[1];
+    }
+    else if (n == 1)
+    {
+        (arr[i - n - 1])->next = NULL;
+    }
+    else (arr[i - n - 1])->next = (arr[i - n])->next;
+
+  	return arr[0];
+}
