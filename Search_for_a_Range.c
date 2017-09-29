c52e4182f4137b69a5802dc29a9640cb422407ca
mifan <yanyn07@gmail.com>
2017-09-29 10:30:51 +0800

    Search for a Range
    Using binary search to locate the position of target.

--- Search_for_a_Range.c
+++ Search_for_a_Range.c
@@ -21,22 +21,43 @@ int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
 	*returnSize = 2;
     if (!numsSize)
 		return arr;
+	int begin = 0, end = numsSize - 1;
 
-	for (i = 0; i < numsSize; i++)
+	while(1)
 	{
-		if (nums[i] == target)
+		mid = (begin + end)/2;
+		if (nums[begin] > target || nums[end] < target)
+			return arr;
+		if (begin == end)
 		{
-			if (j == 0)
+			if (nums[begin] == target)
 			{
-				arr[0] = i;
-                arr[1] = i;
-				j++;
+				arr[0] = begin;
+				arr[1] = end;
 			}
-			else arr[1] = i;
-			*returnSize = 2;
+			else
+			{
+				arr[0] = -1;
+				arr[1] = -1;
+			}
+			return arr;
+		}	
+		else if (nums[mid] == target)
+		{
+			i = mid;
+			j = mid;
+			while(nums[i] == target && i >= 0) i--;
+			while(nums[j] == target && j < numsSize) j++;
+			arr[0] = i + 1;
+			arr[1] = j - 1;
 		}
-        else if (nums[i] > target)
-            break;
+		else if (nums[mid] < target)
+		{
+			begin = mid + 1;
+		}
+		else
+		{
+			end = mid;
+		}
 	}
-	return arr;
 }
